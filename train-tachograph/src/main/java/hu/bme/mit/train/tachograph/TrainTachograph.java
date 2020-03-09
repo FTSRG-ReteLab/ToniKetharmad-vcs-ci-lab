6fd9b0fbba27e615604941ade6d920316abe2cfd
Szkupien Peter <peti.szkupien@gmail.com>
2020-03-09 09:17:26 -0700

    imports added

--- train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/TrainTachograph.java
@@ -7,6 +7,11 @@ import hu.bme.mit.train.interfaces.TrainUser;
 import hu.bme.mit.train.sensor.TrainSensorImpl;
 import hu.bme.mit.train.user.TrainUserImpl;
 
+import com.google.common.collect.ArrayTable;
+import com.google.common.collect.Table;
+
+import java.util.List;
+
 public class TrainTachograph {
 
 	private Table<String, String, Integer> table;
